1d9bf48e6c4ece2038f640f18f8f5b281cbf916f
Yutaka Kobayashi <48763691+kobayashiyutaka@users.noreply.github.com>
2024-07-02 17:25:13 +0900

    Add python scripts

---
+++ three.py
@@ -0,0 +1,28 @@
+# %%
+j = 1
+for i in range(1,1000):
+    hundreds = i // 100
+    tens = (i % 100) // 10
+    ones = i % 10
+    if hundreds == 3 or tens == 3 or ones == 3:
+        if j % 8 == 0:
+            end_str = '\n'
+        else:
+            end_str = ' '
+        print(i,end=end_str)
+        j += 1
+
+# %%
+for i in range(1,1000):
+    s = 0
+    for j in range(1,i):
+        if i % j == 0:
+            s += j
+    if i == s:
+        print(i,s,'完全数')
+    elif s == 1:
+        print(i,s,'素数')
+    else:
+        print(i,s)
+
+# %%
