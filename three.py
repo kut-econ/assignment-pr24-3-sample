209e30a594b7c06ec410b83c2e12744921aea797
Yutaka Kobayashi <48763691+kobayashiyutaka@users.noreply.github.com>
2024-07-03 09:12:42 +0900

    Update README

--- three.py
+++ three.py
@@ -1,26 +1,44 @@
 # %%
-j = 1
-for i in range(1,1000):
-    hundreds = i // 100
-    tens = (i % 100) // 10
-    ones = i % 10
-    if hundreds == 3 or tens == 3 or ones == 3:
-        if j % 8 == 0:
-            end_str = '\n'
-        else:
-            end_str = ' '
-        print(i,end=end_str)
-        j += 1
+prime_numbers = []
+i = 2
+while len(prime_numbers) < 100:
+    for j in range(2,i):
+        if i % j == 0:
+            break
+    else:
+        prime_numbers.append(i)
+    i += 1
+# %%
+for i in range(len(prime_numbers)):
+    if i % 10 == 9:
+        print(prime_numbers[i])
+    else:
+        print(prime_numbers[i],end=' ')
 
 # %%
-for i in range(1,1000):
-    s = 0
-    for j in range(1,i):
-        if i % j == 0:
-            s += j
-    if i == s:
-        print(i,s,'完全数')
-    elif s == 1:
-        print(i,s,'素数')
+rev = prime_numbers[::-1]
+for i in range(len(rev)):
+    if i % 10 == 9:
+        print(rev[i])
     else:
-        print(i,s)
+        print(rev[i],end=' ')
+# %%
+even = prime_numbers[1::2]
+for i in range(len(even)):
+    if i % 10 == 9:
+        print(even[i])
+    else:
+        print(even[i],end=' ')
+
+# %%
+j = 0
+for i in range(len(prime_numbers)):
+    if i+1 in prime_numbers:
+        j += 1
+        if j % 5 == 0:
+            print(prime_numbers[i])
+        else:
+            print(prime_numbers[i],end=' ')
+
+
+# %%
