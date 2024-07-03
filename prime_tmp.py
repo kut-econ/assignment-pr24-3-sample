67f180e2c641f7b4b6721a175b305a5fe158eeea
Yutaka Kobayashi <48763691+kobayashiyutaka@users.noreply.github.com>
2024-07-03 10:35:35 +0900

    Rename prime.py to prime_tmp.py
    And add prime.py, and copied the contents of prime_tmp.py to prime.py

--- prime_tmp.py
+++ prime_tmp.py
@@ -1,8 +1,4 @@
-# 課題で提示されたタスクを実行するコードを以下に記述しなさい。
-# タスクごとにセルを分けて記述してください。
-#%%
-# Task 1. 素数リストprime_numbersの作成と出力
-
+# %%
 prime_numbers = []
 i = 2
 while len(prime_numbers) < 100:
@@ -12,28 +8,21 @@ while len(prime_numbers) < 100:
     else:
         prime_numbers.append(i)
     i += 1
-
-# prime_numbersの要素を出力
+# %%
 for i in range(len(prime_numbers)):
     if i % 10 == 9:
         print(prime_numbers[i])
     else:
         print(prime_numbers[i],end=' ')
-# %%
-# Task 2. prime_numbersの要素を逆順に出力
 
-# prime_numbersを逆順にしたリストを作成
+# %%
 rev = prime_numbers[::-1]
 for i in range(len(rev)):
     if i % 10 == 9:
         print(rev[i])
     else:
         print(rev[i],end=' ')
-
 # %%
-# Task 3. prime_numbersの要素のうち偶数番目のものを出力
-
-# 偶数番目の素数からなるリストを作成
 even = prime_numbers[1::2]
 for i in range(len(even)):
     if i % 10 == 9:
@@ -42,8 +31,6 @@ for i in range(len(even)):
         print(even[i],end=' ')
 
 # %%
-# Task 4. prime_numbersの要素のうち素数番目のものを出力
-
 j = 0
 for i in range(len(prime_numbers)):
     if i+1 in prime_numbers:
@@ -52,3 +39,6 @@ for i in range(len(prime_numbers)):
             print(prime_numbers[i])
         else:
             print(prime_numbers[i],end=' ')
+
+
+# %%
